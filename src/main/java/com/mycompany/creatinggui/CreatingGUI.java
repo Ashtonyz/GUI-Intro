7ff8c32a61ce14f3e3fa8db85c078a6e7dac1627
Ashtonyz <ST10510941@myemeris.edu.za>
2026-02-25 09:30:24 +0200

    GUI Intro

---
+++ src/main/java/com/mycompany/creatinggui/CreatingGUI.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.creatinggui;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author lab_services_student
+ */
+public class CreatingGUI {
+
+    public static void main(String[] args) {
+      
+        JOptionPane.showMessageDialog(null, "Gabe is my class rep!");
+        
+        JOptionPane.showMessageDialog(null, "Programming is fun", "Programs", 1);
+        
+        JOptionPane.showMessageDialog(null, "Taxi Drivers are non-compliant assholes", "Taxi Drivers",2);
+        
+        JOptionPane.showMessageDialog(null, "Dividing by zero is not allowed", "Division By Zero", 0);
+    }
+}
